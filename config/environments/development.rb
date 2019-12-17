14a36e57c936e94baebcde099823b7f17acace80
shogo-hr <harada.tosho.2016@gmail.com>
2019-12-18 00:13:17 +0900

    enable webpack-dev-server

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,8 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  config.x.webpacker[:dev_server_host] = "http://localhost:8080"
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
